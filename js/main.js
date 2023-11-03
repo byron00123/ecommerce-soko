7f7b42bdf1a67b5d7e18ce6ab5e678cfae8ff83f
byron00123 <omondibyron25@gmail.com>
2023-11-03 15:02:32 +0300

    :iphone:add responsive design

--- js/main.js
+++ js/main.js
@@ -12,3 +12,27 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
     cart.classList.remove("active");
 }
+
+//making Add to cart
+//Cart Working JS
+if(document.readyState  == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+}else{
+    ready();
+}
+
+//Making Function
+function ready(){
+    //Remove Item From Cart
+    var reemoveCartButtons = document.getElementsByClassName('cart-remove');
+    for (var i = 0; i < reemoveCartButtons.length; i++){
+        var button = reemoveCartButtons[i];
+        button.addEventListener('click', removeCartItem);
+    }
+}
+
+//Remove Cart Item
+function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+}
